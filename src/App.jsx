416ed7ae45b22e7f88068582df35ed1133be5c75
himanshu <himanshuchauhan696@gmail.com>
2024-09-27 13:54:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Demo from "./components/Demo"
+
+import Demo2 from "./components/Demo2"
+
+
+export default function App() {
+  return (
+    <>
+    <h1 className="text-4xl font-bold underline bg-orange-200">
+      Hello world!
+
+  
+    </h1>
+        <Demo/>
+        <Demo2/>
+
+        
+       
+        </>
+  )
+}
---
+++ src/components/Demo2.jsx
@@ -0,0 +1,20 @@
+import {Component} from "react";
+
+class Demo2 extends Component {
+
+    render() {
+return (
+
+<div>
+    <h1>Hello, this is my class component! </h1>
+
+</div>
+
+
+);
+
+    }
+
+}
+
+export default Demo2;
